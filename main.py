cc0a1b42bee9dbec72f90b018ff1e9c12e60a376
emailmeydrive <emailme.ydrive@gmail.com>
2022-04-06 14:18:08 +0300

    added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pygame 
+import pygame
 import time
 
-# screen size 
-WINDOW_W = 1000
+# screen size
+WINDOW_W = 600
 WINDOW_H = 600
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 
@@ -11,27 +11,31 @@ screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Tetris")
 
 bk_image = pygame.image.load("tet.jpg")
-
+shape_g = pygame.image.load("shape.G..png")
+shape = pygame.image.load("shape.G.png")
+shape_i = pygame.image.load("shape.I.png")
+shape_les = pygame.image.load("shape.les.png")
+shape_t = pygame.image.load("shape.T.png")
 
 # Initialing Color
-color = (255,100,200)
-  
+color = (255, 100, 200)
+
 # Drawing Rectangle
 
 play = True
-while  play:
+while play:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            play=False
-         
-    screen.blit(bk_image,(0,0))
+            play = False
 
-                                            #  X    Y   H   W
-    pygame.draw.rect(screen,color,pygame.Rect( 70, 30, 60, 60))
+    screen.blit(bk_image, (0, 0))
+    screen.blit(shape_g, (WINDOW_W/2+50, 0))
+    screen.blit(shape_i, (WINDOW_W/2-80, 0))
+    screen.blit(shape_les, (WINDOW_W/2-150, 0))
+    screen.blit(shape_t, (WINDOW_W/2+100, 0))
+    screen.blit(shape, (WINDOW_W/2+100, 0))
 
     pygame.display.flip()
-
-
 
 
 pygame.quit()
